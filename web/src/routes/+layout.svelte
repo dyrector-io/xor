89ae8bff18de5e33cd9bb2cee60fac05d74a7fe1
Levente Orban <levente.orban@dyrector.io>
2022-12-17 17:50:48 +0100

    feat: add simple question type

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<slot />
+
+<style>
+	:global(body) {
+		font-family: helvetica, arial, sans-serif;
+		color: #fefefe;
+		background: rgb(0, 45, 69);
+
+		margin: 0 auto;
+	}
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	const END_POINT = 'URL';
+
+    let obj: Question
+    let answer: string
+
+    type Question = {
+        question: string,
+        answer: string,
+        starsNumber: number,
+        field: string
+    }
+
+	onMount(() => {
+        getQuiz()
+	});
+
+	function getQuiz(): Question {
+        const q: Question = {
+            question: "Kiscica?",
+            answer: "Aladar",
+            starsNumber: 2121,
+            field: "Service Meash"
+        }
+
+        obj = q
+
+        return q
+		// return fetch(`URL`)
+		// 	.then((resp) => resp.json())
+		// 	.then((json) => init(json))
+		// 	.catch((err) => console.log(err.message))
+	}
+
+
+	function checkAnswer(): any {
+	}
+
+    function init() {
+        const obj: Question = getQuiz()
+        console.log(obj)
+    }
+
+</script>
+
+<div class="">
+    <h1 class="text-3xl font-bold">XOR Quiz</h1>
+    <div>{JSON.stringify(obj)}</div>
+    <input class="text-black" bind:value={answer}>
+</div>
