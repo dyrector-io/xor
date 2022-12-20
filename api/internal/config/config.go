e304dcd686c101c8ea7c30947ccdd1e53b899ef1
Nandor Magyar <nandormagyar.it@gmail.com>
2022-12-20 18:11:07 +0100

    landscape json filtered, origin in envs

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -6,10 +6,11 @@ import (
 )
 
 type AppConfig struct {
-	Port   uint16 `env:"PORT" env-default:"3333"`
-	Debug  bool   `env:"DEBUG"`
-	DSN    string `env:"DSN"`
-	Method string `env:"METHOD" env-default:"RANDOM"`
+	AllowedOrigins string `env:"ALLOWED_ORIGINS"`
+	Port           uint16 `env:"PORT" env-default:"3333"`
+	Debug          bool   `env:"DEBUG"`
+	DSN            string `env:"DSN"`
+	Method         string `env:"METHOD" env-default:"RANDOM"`
 }
 type AppState struct {
 	AppConfig *AppConfig
--- api/pkg/server/server.go
+++ api/pkg/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/dyrector-io/xor/api/internal/config"
@@ -42,15 +43,21 @@ func GetChi(appConfig *config.AppConfig) *http.Server {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
+	origins := []string{}
+	if appConfig.Debug {
+		origins = append(origins, "http://*", "https://*")
+	}
+	if appConfig.AllowedOrigins != "" {
+		origins = append(origins, strings.Split(appConfig.AllowedOrigins, ",")...)
+	}
+	log.Info().Msgf("allowed origins: %v", origins)
 	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedOrigins:   origins,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		MaxAge:           300,
 	}))
 
 	db := database.InitPostgres(appConfig)
